8c3d828cf46fef5dea5c8e0b39b239e3703e11ec
Naiem_Devs <46394592+Naiem-Devs@users.noreply.github.com>
2022-03-23 15:19:19 +0600

    all-done

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,5 +16,31 @@
     }
 });
 
+  // owlCarousel
+  $(".blog_slide").owlCarousel({
+    loop: true,
+    smartSpeed: 500,
+    margin: 30,
+    items: 3,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+
+  $('select').niceSelect();
  
 })(jQuery);
